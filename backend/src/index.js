c38fda41569b266f363777054dec6acb7c4bfd6b
nszo <leanmuag@outlook>
2024-01-09 10:51:58 -0500

    feat(Backend):mysql back connection

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,4 +1,4 @@
-import app from "./app";
+import app from './app'
 
-app.listen(3000);
-console.log("listening on port 3000");
+app.listen(app.get('port'))
+console.log('Server on port', app.get('port'))
